4e0722c319e41a8b6c1ae262f6a8a0a5c14a58f2
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2021-12-10 14:41:58 +0000

    Add production env file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,30 @@
-import { defineConfig, searchForWorkspaceRoot } from "vite";
+import { defineConfig, searchForWorkspaceRoot, UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import path from "path";
 
-export default defineConfig(({ command }) => ({
-  base:
-    command === "build"
-      ? "/language-revisor/" // base path when deployed on github pages
-      : undefined,
+export default defineConfig(({ mode }) => {
+  const config: UserConfig =
+    mode === "production"
+      ? {
+          base: "/language-revisor/",
+          envDir: "../.github/",
+        }
+      : {};
 
-  build: { outDir: "build" },
-
-  plugins: [vue(), vueJsx()],
-
-  resolve: {
-    alias: {
-      "@/common": path.resolve(__dirname, "../common"),
+  return {
+    ...config,
+    build: { outDir: "build" },
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        "@/common": path.resolve(__dirname, "../common"),
+      },
     },
-  },
-
-  server: {
-    fs: {
-      allow: [searchForWorkspaceRoot(process.cwd()), "../common"],
+    server: {
+      fs: {
+        allow: [searchForWorkspaceRoot(process.cwd()), "../common"],
+      },
     },
-  },
-}));
+  };
+});
